92bd2ccc3ddc0ab69508551c7e148ac3d6b2a171
Olivier <looolive@gmail.com>
2023-01-10 16:28:13 +0100

    viewsets

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-from .views import ProjectList, ProjectDetail
+# from django.urls import path
+from rest_framework.routers import SimpleRouter
+# from .views import ProjectList, ProjectDetail
+from .views import ProjectViewSet
 
-urlpatterns = [
-    path("projects/<int:pk>/", ProjectDetail.as_view(), name="project_detail"),
-    path("projects/", ProjectList.as_view(), name="project_list"),
-]
+# urlpatterns = [
+#     path("projects/<int:pk>/", ProjectDetail.as_view(), name="project_detail"),
+#     path("projects/", ProjectList.as_view(), name="project_list"),
+# ]
+
+router = SimpleRouter()
+router.register("projects", ProjectViewSet, basename="projects")
+
+urlpatterns = router.urls
--- tickets/views.py
+++ tickets/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework import viewsets
 
 from .serializers import UserSignupSerializer
 from .models import Project
@@ -7,7 +8,7 @@ from .permissions import IsAuthorOrContributor
 from .serializers import ProjectSerializer
 
 
-class ProjectList(generics.ListCreateAPIView):
+class ProjectViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrContributor,)
 
     def get_queryset(self):
@@ -22,19 +23,34 @@ class ProjectList(generics.ListCreateAPIView):
     serializer_class = ProjectSerializer
 
 
-class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthorOrContributor,)
-
-    def get_queryset(self):
-        """
-        List projects where authenticated user is author or contributor
-        """
-        user = self.request.user
-        query_contributor = user.contributor.all()
-        projects = Project.objects.filter(author=user) | Project.objects.filter(contributor__in=query_contributor)
-        return projects.distinct()
-
-    serializer_class = ProjectSerializer
+# class ProjectList(generics.ListCreateAPIView):
+#     permission_classes = (IsAuthorOrContributor,)
+#
+#     def get_queryset(self):
+#         """
+#         List projects where authenticated user is author or contributor
+#         """
+#         user = self.request.user
+#         query_contributor = user.contributor.all()
+#         projects = Project.objects.filter(author=user) | Project.objects.filter(contributor__in=query_contributor)
+#         return projects.distinct()
+#
+#     serializer_class = ProjectSerializer
+#
+#
+# class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
+#     permission_classes = (IsAuthorOrContributor,)
+#
+#     def get_queryset(self):
+#         """
+#         List projects where authenticated user is author or contributor
+#         """
+#         user = self.request.user
+#         query_contributor = user.contributor.all()
+#         projects = Project.objects.filter(author=user) | Project.objects.filter(contributor__in=query_contributor)
+#         return projects.distinct()
+#
+#     serializer_class = ProjectSerializer
 
 
 class RegisterView(generics.CreateAPIView):
